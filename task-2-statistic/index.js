241d6859e46cacdc2b3191ecb09959d51c1ad4b5
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2022-11-20 13:02:07 +0200

    task 2

--- task-2-statistic/index.js
+++ task-2-statistic/index.js
@@ -5,12 +5,40 @@
 // - кількість елементів кожного тегу
 // Для роботи з класами рекомендується використовувати classList
 
+const containerMid = document.querySelector(".container-mid");
+
 function collectDOMStat(root) {
   const stat = {
     tags: {},
     classes: {},
     texts: 0,
   };
+  const childNodes = containerMid.childNodes;
+  childNodes.forEach((node) => {
+    if (node.nodeType === 3) {
+      stat.texts += 1;
+    } else if (node.nodeType === 1) {
+      const tag = node.tagName;
+      const classList = node.classList;
 
+      classList.forEach((c) => {
+        if (stat.classes[c]) {
+          stat.classes[c] += 1;
+        } else {
+          stat.classes[c] = 1;
+        }
+      });
+
+      if (stat.tags[tag]) {
+        stat.tags[tag] += 1;
+      } else {
+        stat.tags[tag] = 1;
+      }
+    }
+  });
+
+  console.log(childNodes);
   return stat;
 }
+
+console.log(collectDOMStat(containerMid));
