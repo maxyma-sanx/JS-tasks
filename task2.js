2059c67c106106a028ead452f562c681cd20100c
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2022-12-04 12:49:03 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,11 +1,25 @@
 //Напишіть функцію, яка виводитиме числа від 1 до 10 із затримкою в 1 секунду між кожним значенням
 
-function count() {
-  for (let i = 1; i <= 10; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
-  }
+// function count() {
+//   for (let i = 1; i <= 10; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+
+// count();
+
+function count2() {
+  let number = 1;
+
+  const id = setInterval(() => {
+    console.log(number);
+    number += 1;
+    if (number > 10) {
+      clearInterval(id);
+    }
+  }, 1000);
 }
 
-count();
+count2();
